7c97ce8100be5ad7681827e80bb1de36d90899e2
Kurt Miller <kmiller9393@gmail.com>
2018-09-30 13:30:42 -0600

    Update methods so that mockData is visible at /json and error is displayed at /error file paths

--- server.js
+++ server.js
@@ -1,19 +1,18 @@
 const express = require('express');
 const data = require('./public/mockData.json');
-
 const app = express();
 
-const urlLogger = (request, next) => {
+const urlLogger = (request, response, next) => {
   console.log('Request URL:', request.url);
   next();
 };
 
-const timeLogger = next => {
+const timeLogger = (request, response, next) => {
   console.log('Datetime:', new Date(Date.now()).toString());
   next();
 };
 
-const displayError = response => {
+const displayError = (request, response, next) => {
   response.status(404).send("We can't seem to find what you're looking for!");
 };
 
@@ -23,7 +22,7 @@ app.use(express.static('public'));
 
 app.get('/error', displayError);
 
-app.get('/json', response => {
+app.get('/json', (request, response) => {
   response.status(200).json(data);
 });
 
